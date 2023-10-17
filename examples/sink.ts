0a8f445904cbed98a705e4ca373b0b1ee631c223
stagas <gstagas@gmail.com>
2023-10-18 00:29:01 +0300

    save

--- examples/sink.ts
+++ examples/sink.ts
@@ -33,4 +33,5 @@ world.canvas.appendTo(dom.body)
 const scene = $(new Editor(world))
 const source = $(new Source(tokenize))
 source.code = 'hello world'
+scene.ctx!.buffer.source = source
 scene.draw()
