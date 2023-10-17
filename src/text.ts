29cc9095147a3bcdaf93fdcb481ea8363c9c9fde
stagas <gstagas@gmail.com>
2023-10-18 01:34:08 +0300

    save

--- src/text.ts
+++ src/text.ts
@@ -84,7 +84,7 @@ export class TextScene extends RenderScene {
           = c.strokeStyle
           = '#fff'
           // = skin.colors[color] ?? '#fff'
-
+        console.log(t.text)
         c.fillText(t.text, x, y)
         c.strokeText(t.text, x, y)
       }
