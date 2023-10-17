8b536456ea443e3f79fc99395ce62093930db675
stagas <gstagas@gmail.com>
2023-10-18 00:12:28 +0300

    save

--- examples/sink.ts
+++ examples/sink.ts
@@ -3,6 +3,18 @@ import { World } from 'std'
 import { dom } from 'utils'
 import { Editor } from '../src/editor.ts'
 
+const style = document.createElement('style')
+dom.head.append(style)
+style.textContent = /*css*/`
+html, body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+`
+
 const world = $(new World)
 world.canvas.appendTo(dom.body)
 const scene = $(new Editor(world))
