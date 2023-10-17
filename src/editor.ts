6c8e964daaf8a01d157927e1882d661bba9caff4
stagas <gstagas@gmail.com>
2023-10-18 01:55:27 +0300

    save

--- src/editor.ts
+++ src/editor.ts
@@ -8,7 +8,6 @@ import { World } from 'std'
 export class Editor extends RenderScene {
   constructor(public world: World) {
     super(world)
-    console.log(this.rect)
     this.ctx = $(new Context(world, this.rect))
     this.text = $(new TextScene(this.ctx))
   }
