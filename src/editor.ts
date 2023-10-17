8e0d7aabe7a2bf8ab0de3f1c8dad5ad2f58f6da9
stagas <gstagas@gmail.com>
2023-10-18 00:49:00 +0300

    save

--- src/editor.ts
+++ src/editor.ts
@@ -22,7 +22,7 @@ export class Editor extends RenderScene {
     rect.resizeToWindow()
   }
   render() { }
-  draw() {
+  @fn draw() {
     const { rect, world, scenes } = $.of(this)
     const { canvas } = $.of(world)
     const { c } = canvas
