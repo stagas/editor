efb8fe2c007433c75683713a9304bf36cd804dd3
stagas <gstagas@gmail.com>
2023-10-18 20:13:50 +0300

    save

--- src/editor.ts
+++ src/editor.ts
@@ -40,7 +40,7 @@ export class Editor extends Render {
   history = $(new History(this))
   buffer = $(new Buffer(this, { Type: {} }))
   scroll = $(new Scroll(this))
-  dims = $(new Dims(this))
+  get dims() { return $(new Dims(this)) }
   input = $(new Input(this))
   selection = $(new Selection(this))
   text = $(new Text(this))
