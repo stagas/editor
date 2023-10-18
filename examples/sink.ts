153437c52cc38145ae494dc525877ba9da95170b
stagas <gstagas@gmail.com>
2023-10-18 19:34:37 +0300

    save

--- examples/sink.ts
+++ examples/sink.ts
@@ -28,7 +28,7 @@ const code = String.raw`{ x:=
 `.repeat(10)
 
 function tokenize({ code }: { code: string }) {
-  return [...code.matchAll(/(?<word>[^\s\n]+)|(?<space>[\s\n]+)/gmi)]
+  return [...code.matchAll(/(?<word>[^\s\n]+)|(?<space>[\s\n]+)/g)]
     .map(m => {
       return {
         type: 'text',
