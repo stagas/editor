27b5ae2e7c2e8e14d1299e00d099f4ef24acd375
stagas <gstagas@gmail.com>
2023-10-18 12:27:36 +0300

    save

--- src/text.ts
+++ src/text.ts
@@ -32,8 +32,6 @@ export class TextScene extends RenderScene {
     const { rect } = $.of(this.ctx)
     const { hasSize, size: { wh } } = $.when(rect)
     this.rect.setSize(wh)
-    $.untrack()
-    this.needRender = true
   }
 
   @fx trigger_render() {
