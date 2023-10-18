8ca2d59e68eff36cac32de04feb31ed050d5dfa2
stagas <gstagas@gmail.com>
2023-10-18 13:25:20 +0300

    save

--- src/editor.ts
+++ src/editor.ts
@@ -8,7 +8,7 @@ import { World } from 'std'
 export class Editor extends Render {
   constructor(public world: World) {
     super(world)
-    this.canvas = world.canvas
+    this.canvas.fullWindow = true
     this.ctx = $(new Context(world, this.rect))
     this.text = $(new Text(this.ctx))
   }
@@ -19,11 +19,11 @@ export class Editor extends Render {
     const { text } = $.of(this)
     return [text]
   }
-  // @fx resize_editor_to_world() {
-  //   const { rect, canvas } = $.of(this)
-  //   const { size } = $.of(canvas)
-  //   rect.setSize(size)
-  // }
+  @fx resize_editor_to_world() {
+    const { rect, canvas } = $.of(this)
+    const { size } = $.of(canvas)
+    rect.setSize(size)
+  }
   @fx maybe_needDraw() {
     const { scenes } = $.of(this)
     let d = false
@@ -40,14 +40,13 @@ export class Editor extends Render {
     requestAnimationFrame(() => this.draw())
   }
   @fn initCanvas() {
-    const { c } = $.of(this.world.canvas)
+    const { c } = $.of(this.canvas)
     c.imageSmoothingEnabled = false
   }
   @fn render() {
   }
   @fn draw() {
-    const { rect, world, scenes } = $.of(this)
-    const { canvas } = $.of(world)
+    const { rect, scenes, canvas } = $.of(this)
     const { c } = canvas
     rect.fill(c, '#224')
     for (const scene of scenes) {
