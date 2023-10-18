8b69013e80c52fbded62cd9af3b650be6c0a61d0
stagas <gstagas@gmail.com>
2023-10-18 13:29:03 +0300

    save

--- src/editor.ts
+++ src/editor.ts
@@ -9,6 +9,7 @@ export class Editor extends Render {
   constructor(public world: World) {
     super(world)
     this.canvas.fullWindow = true
+    this.rect.size = this.canvas.size
     this.ctx = $(new Context(world, this.rect))
     this.text = $(new Text(this.ctx))
   }
@@ -19,11 +20,11 @@ export class Editor extends Render {
     const { text } = $.of(this)
     return [text]
   }
-  @fx resize_editor_to_world() {
-    const { rect, canvas } = $.of(this)
-    const { size } = $.of(canvas)
-    rect.setSize(size)
-  }
+  // @fx resize_editor_to_world() {
+  //   const { rect, canvas } = $.of(this)
+  //   const { size } = $.of(canvas)
+  //   rect.setSize(size)
+  // }
   @fx maybe_needDraw() {
     const { scenes } = $.of(this)
     let d = false
