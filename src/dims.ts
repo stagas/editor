c879f463dbf193789a9fb4ccdb7918826656152f
stagas <gstagas@gmail.com>
2023-10-18 14:10:30 +0300

    save

--- src/dims.ts
+++ src/dims.ts
@@ -21,7 +21,7 @@ export class Dims {
   // }
 
   get lines() {
-    return $.of($.of(this.ctx).buffer).source.lines
+    return $.of(this.ctx).buffer?.source?.lines
   }
   // @fx update_lines() {
   //   const { lines } = $.of($.of($.of(this.ctx).buffer).source)
