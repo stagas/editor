ee65f968e75b14e43cab90e206dfab2156d1e92a
stagas <gstagas@gmail.com>
2023-10-18 15:19:37 +0300

    save

--- src/context.ts
+++ src/context.ts
@@ -9,7 +9,6 @@ import { History } from './history.ts'
 import { Input } from './input.ts'
 import { Selection } from './selection.ts'
 import { WidgetLike } from './widgets.ts'
-import { Text } from './text.ts'
 
 interface PointerItem { }
 
@@ -30,48 +29,31 @@ export class Context extends Scene {
     super(world)
   }
 
+  misc!: $<Misc>
+  skin!: Skin
+  colors!: Colors
+  history!: $<History>
+  buffer!: $<Buffer>
+  scroll!: $<Scroll>
+  dims!: $<Dims>
+  input!: $<Input>
+  selection!: $<Selection>
+  text!: $<Text>
+
   sub: (WidgetLike | (WidgetLike & PointerItem))[] = []
   deco: WidgetLike[] = []
 
-  misc = $(new Misc)
-  skin = { colors: {
-    bgBright2: '#555'
-  }, fonts: { mono: 'monospace' } }
-  colors = {}
-  history = $(new History(this))
-  buffer = $(new Buffer(this, { Type: {} }))
-  scroll = $(new Scroll(this))
-  dims = $(new Dims(this))
-  input = $(new Input(this))
-  selection = $(new Selection(this))
-  text = $(new Text(this))
-
-  // misc!: $<Misc>
-  // skin!: Skin
-  // colors!: Colors
-  // history!: $<History>
-  // buffer!: $<Buffer>
-  // scroll!: $<Scroll>
-  // dims!: $<Dims>
-  // input!: $<Input>
-  // selection!: $<Selection>
-  // text!: $<Text>
-
-  // sub: (WidgetLike | (WidgetLike & PointerItem))[] = []
-  // deco: WidgetLike[] = []
-
-  // @init init() {
-  //   this.misc = $(new Misc)
-  //   this.skin = { colors: {
-  //     bgBright2: '#555'
-  //   }, fonts: { mono: 'monospace' } }
-  //   this.colors = {}
-  //   this.history = $(new History(this))
-  //   this.buffer = $(new Buffer(this, { Type: {} }))
-  //   this.scroll = $(new Scroll(this))
-  //   this.dims = $(new Dims(this))
-  //   this.input = $(new Input(this))
-  //   this.selection = $(new Selection(this))
-  //   this.text = $(new Text(this))
-  // }
+  @init init() {
+    this.misc = $(new Misc)
+    this.skin = { colors: {
+      bgBright2: '#555'
+    }, fonts: { mono: 'monospace' } }
+    this.colors = {}
+    this.history = $(new History(this))
+    this.buffer = $(new Buffer(this, { Type: {} }))
+    this.scroll = $(new Scroll(this))
+    this.dims = $(new Dims(this))
+    this.input = $(new Input(this))
+    this.selection = $(new Selection(this))
+  }
 }
