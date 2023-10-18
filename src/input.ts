5e07bb428cc310387384b195685065c271f9c569
stagas <gstagas@gmail.com>
2023-10-18 18:47:09 +0300

    save

--- src/input.ts
+++ src/input.ts
@@ -9,7 +9,7 @@ export class Input extends Comp {
   keyboard = $(new Keyboard(this.ctx))
   mouse = $(new Mouse(this.ctx))
 
-  textareaRect = $(new Rect, { w: 50, h: 50 })
+  textareaRect = $(new Rect, { center: this.ctx.world.pointer.$.pos, w: 50, h: 50 })
   textarea: HTMLTextAreaElement = dom.el('textarea', {
     spellcheck: false,
     autocorrect: 'off',
