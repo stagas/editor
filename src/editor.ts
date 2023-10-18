5d09b73860d8de6129c7a2221d2696ec73086a90
stagas <gstagas@gmail.com>
2023-10-18 20:05:41 +0300

    save

--- src/editor.ts
+++ src/editor.ts
@@ -90,7 +90,7 @@ export class Editor extends Render {
       Math.round(scrollPos.top) !== targetScroll.top ||
       Math.round(scrollPos.left) !== targetScroll.left
     $.untrack()
-    console.log(needAnim)
+    console.log(targetScroll.text)
     if (needAnim) {
       this.needUpdate = true
     }
