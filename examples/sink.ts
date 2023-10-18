8eb043f272e03ad3eb845659e3f408d6972228a2
stagas <gstagas@gmail.com>
2023-10-18 17:17:47 +0300

    save

--- examples/sink.ts
+++ examples/sink.ts
@@ -39,6 +39,6 @@ $.batch(() => {
   source.code = 'hello world'
 
   editor.buffer.source = source
-  editor.buffer.col = editor.buffer.coli = 3
+  editor.buffer.coli = 3
   // editor.selection.end.set({ x: 3, y: 0 })
 })
