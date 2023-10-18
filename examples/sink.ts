ae8bac59e2ab22ee82196ec51072b6d5223a452c
stagas <gstagas@gmail.com>
2023-10-18 19:53:00 +0300

    save

--- examples/sink.ts
+++ examples/sink.ts
@@ -28,20 +28,14 @@ const code = String.raw`{ x:=
 `.repeat(10)
 
 function tokenize({ code }: { code: string }) {
-  return [...code.matchAll(/(?<word>[^\s]+)|(?<space>[\s]+)/g)]
-    .map(({ 0: text, index }) => ({
+  let line = 0
+  let col = 0
+  return [...code.matchAll(/(?<word>[^\s]+)|(?<newline>\n)|(?<space>[\s]+)/g)]
+    .map(({ 0: text, groups: { newline } }: any) => ({
       type: 'text',
-      text,
-      line: code.slice(0, index).split('\n').length - 1,
-      col: Math.max(0, index! - code.lastIndexOf('\n', index) - 1),
-
-      // col: Math.max(0,
-      //   m.index!
-      //   - code.slice(0, m.index)
-      //     .split('\n')
-      //     .slice(0, -1)
-      //     .join('\n').length - 1
-      // ),
+      text: newline ? newline : (col += text.length, text),
+      line: newline ? (col = 0, ++line) : line, //code.slice(0, index).split('\n').length - 1,
+      col //: Math.max(0, index! - code.lastIndexOf('\n', index) - 1),
     }))
 }
 
