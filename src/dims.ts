3500a47308ad6093df5bafa60c7848f982b8dcd6
stagas <gstagas@gmail.com>
2023-10-18 10:13:12 +0300

    save

--- src/dims.ts
+++ src/dims.ts
@@ -19,10 +19,10 @@ export class Dims {
   lines?: string[]
   @fx update_lines() {
     const { lines } = $.of($.of($.of(this.ctx).buffer).source)
-    // $.untrack()
+    $.untrack()
     //!: update_lines
     console.warn([...lines])
-    this.lines = lines
+    // this.lines = lines
   }
   sub: (WidgetLike | (WidgetLike & PointerItem))[] = []
   deco: WidgetLike[] = []
