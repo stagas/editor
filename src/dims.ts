27788b299af12a1191c7571e6a90c2e3b8b931c2
stagas <gstagas@gmail.com>
2023-10-18 14:30:46 +0300

    save

--- src/dims.ts
+++ src/dims.ts
@@ -55,7 +55,7 @@ export class Dims {
   lineHeights: number[] = [0]
 
   @nulls get innerSizeWidth() {
-    const { longestLine, charWidth, scrollbarSize } = $.of(this)
+    const { lines, longestLine, charWidth, scrollbarSize } = $.of(this)
     return longestLine * charWidth + scrollbarSize.w
   }
 
