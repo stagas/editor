8686c52d19a1bc1b809bbc94693185f9bc8f0e8f
stagas <gstagas@gmail.com>
2023-10-19 15:20:56 +0300

    save

--- src/mouse.ts
+++ src/mouse.ts
@@ -38,7 +38,6 @@ export class Mouse extends Comp {
 
       case PointerEventType.Wheel:
         scroll.targetScroll.mulSub(wheel, 0.2)
-        wheel.zero()
         scroll.animScrollStrategy = AnimScrollStrategy.Medium
         break
 
