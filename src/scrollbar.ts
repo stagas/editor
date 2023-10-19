a09f03b08bc61869a2cfba0080a454452222b6c7
stagas <gstagas@gmail.com>
2023-10-19 17:31:19 +0300

    save

--- src/scrollbar.ts
+++ src/scrollbar.ts
@@ -51,7 +51,7 @@ export class Scrollbar extends Render {
     const { dim, ctx, rect: r } = $.of(this)
     const { dims, scroll } = $.of(ctx)
     const { scrollSize } = $.of(scroll)
-    // const { absSum: ab1 } = $.when(scrollSize)
+    const { absSum: ab1 } = $.when(scrollSize)
     const { rect, scrollbarSize } = $.of(dims)
     // const { absSum: ab2 } = $.when(scrollbarSize)
 
