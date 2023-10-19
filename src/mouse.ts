48f33ed73c56b2587ad8d98fe09d2415754defba
stagas <gstagas@gmail.com>
2023-10-19 14:07:18 +0300

    save

--- src/mouse.ts
+++ src/mouse.ts
@@ -57,7 +57,7 @@ export class Mouse extends Comp {
 
         buffer.lineCol.set(lineCol)
         buffer.coli = lineCol.col
-console.log(this.downCount)
+
         switch (this.downCount) {
           case 1:
             selection.resetTo(lineCol)
