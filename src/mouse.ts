b6b0c72f535222f79e0c9a5cf81a4985157d4690
stagas <gstagas@gmail.com>
2023-10-19 12:43:31 +0300

    save

--- src/mouse.ts
+++ src/mouse.ts
@@ -1,13 +1,13 @@
 log.active
 import { $, fn, fx } from 'signal'
-import { Point } from 'std'
+import { Point, PointerEventType } from 'std'
 import { PointerLikeEvent } from 'utils'
 import { Comp } from './comp.ts'
 import { AnimScrollStrategy } from './scroll.ts'
-import { DOUBLE_CLICK_MS } from './constants.ts'
+import { DOUBLE_CLICK_MS, SINGLE_CLICK_MS } from './constants.ts'
 
 export class Mouse extends Comp {
-  hoveringLineCol = $(new Point)
+  lineCol = $(new Point)
 
   isDown = false
   downCount = 0
@@ -15,25 +15,27 @@ export class Mouse extends Comp {
   downTime = 0
 
   @fx handle_pointer_event() {
-    const { ctx, hoveringLineCol: p } = $.of(this)
-    const { world, misc, buffer: b, scroll, selection } = $.of(ctx)
+    const { ctx, lineCol } = $.of(this)
+    const { world, misc, buffer, scroll, selection, input: { textarea } } = $.of(ctx)
     const { pointer } = $.of(world)
     const { time } = pointer
     $._()
     const { event, type, pos, wheel, buttons, alt, ctrl, shift } = pointer
 
-    b.getLineColFromPoint(pos, true, p)
+    ctx.isHovering = pos.withinRect(ctx.rect)
+
+    buffer.getLineColFromPoint(pos, true, lineCol)
 
     switch (type) {
-      case 'move':
+      case PointerEventType.Move:
         break
 
-      case 'wheel':
-        scroll.targetScroll.subMul(wheel, 0.2)
+      case PointerEventType.Wheel:
+        scroll.targetScroll.mulSub(wheel, 0.2)
         scroll.animScrollStrategy = AnimScrollStrategy.Medium
         break
 
-      case 'down':
+      case PointerEventType.Down:
         if (time - this.downTime < DOUBLE_CLICK_MS) {
           this.downCount++
         }
@@ -44,61 +46,64 @@ export class Mouse extends Comp {
         this.downPos.set(pos)
         this.downTime = event.timeStamp
 
-        b.line = p.y
-        b.coli = p.x
+        buffer.lineCol.set(lineCol)
 
         switch (this.downCount) {
           case 1:
-            selection.resetTo(p)
+            selection.resetTo(lineCol)
             break
 
           case 2:
-            if (selection.selectWordBoundary(p)) {
+            if (selection.selectWordBoundary(lineCol)) {
               break
             }
           case 3:
-            if (selection.selectMatchingBrackets(p)) {
+            if (selection.selectMatchingBrackets(lineCol)) {
               break
             }
           case 4:
-            selection.selectLine(p.y)
+            selection.selectLine(lineCol.line)
             break
         }
         break
 
-      case 'up':
+      case PointerEventType.Up:
         this.isDown = false
+        if (time - this.downTime < SINGLE_CLICK_MS) {
+          //on click
+        }
+        textarea.focus()
         break
 
-      case 'leave':
+      case PointerEventType.Leave:
         ctx.isHovering = false
         break
     }
   }
 
-  @fx update_hovering() {
-    const { ctx } = $.of(this)
-    const { world, misc, buffer, scroll } = $.of(ctx)
-    const { pointer } = $.of(world)
-    const { lines } = $.of(buffer)
-    const { x, y } = pointer.pos
-    const { x: sx, y: sy } = scroll.pos
-    $._()
-    ctx.isHovering = pointer.pos.withinRect(ctx.rect)
-    if (misc.isScrolling || misc.wasScrolling || !ctx.isHovering) return
-    buffer.getLineColFromPoint(pointer.pos, true, this.hoveringLineCol)
-  }
-  @fx onWheel() {
-    const { isHovering } = $.when(this.ctx)
-    const { world: { pointer }, scroll } = $.of(this.ctx)
-    const e = pointer.event
-    const { deltaTimeStamp, deltaX, deltaY } = $.of(e)
-    $._()
-    if (e.type !== 'wheel') return
-    scroll.targetScroll.y -= deltaY * 0.2
-    scroll.targetScroll.x -= deltaX * 0.2
-    scroll.animScrollStrategy = AnimScrollStrategy.Medium
-  }
+  // @fx update_hovering() {
+  //   const { ctx } = $.of(this)
+  //   const { world, misc, buffer, scroll } = $.of(ctx)
+  //   const { pointer } = $.of(world)
+  //   const { lines } = $.of(buffer)
+  //   const { x, y } = pointer.pos
+  //   const { x: sx, y: sy } = scroll.pos
+  //   $._()
+  //   ctx.isHovering = pointer.pos.withinRect(ctx.rect)
+  //   if (misc.isScrolling || misc.wasScrolling || !ctx.isHovering) return
+  //   buffer.getLineColFromPoint(pointer.pos, true, this.lineCol)
+  // }
+  // @fx onWheel() {
+  //   const { isHovering } = $.when(this.ctx)
+  //   const { world: { pointer }, scroll } = $.of(this.ctx)
+  //   const e = pointer.event
+  //   const { deltaTimeStamp, deltaX, deltaY } = $.of(e)
+  //   $._()
+  //   if (e.type !== 'wheel') return
+  //   scroll.targetScroll.y -= deltaY * 0.2
+  //   scroll.targetScroll.x -= deltaX * 0.2
+  //   scroll.animScrollStrategy = AnimScrollStrategy.Medium
+  // }
   @fn onPointerUp() {
     const { input: { textarea } } = $.of(this.ctx)
     textarea.focus()
@@ -111,7 +116,7 @@ export class Mouse extends Comp {
   }
   // TODO: holdMove?
   @fn onWindowMove(e: PointerLikeEvent) {
-    const { hoveringLineCol: p, ctx } = $.of(this)
+    const { lineCol: p, ctx } = $.of(this)
     const { buffer: b, selection } = $.of(ctx)
     selection.end.set(p)
     b.line = selection.end.y
@@ -119,7 +124,7 @@ export class Mouse extends Comp {
   }
   @fx onPointerDown() {
     const { isHovering } = $.when(this.ctx)
-    const { hoveringLineCol: p, ctx } = $.of(this)
+    const { lineCol: p, ctx } = $.of(this)
     const { world: { pointer }, buffer: b, selection } = $.of(ctx)
     const { event: e } = $.of(pointer)
 
