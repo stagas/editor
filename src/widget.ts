3fad48652fc46ce74b6e561ec7f7ba88eaa19131
stagas <gstagas@gmail.com>
2023-10-20 21:40:53 +0300

    save

--- src/widget.ts
+++ src/widget.ts
@@ -81,7 +81,8 @@ export class Widget extends Comp {
       @fn draw(t: number, c: CanvasRenderingContext2D) {
         const { rect } = $.of(this)
         rect.fill(c, '#666')
-        // this.needDraw = false
+        console.log('DRAW')
+        this.needDraw = false
       }
     }
     return $(new WidgetRenderable(this.ctx))
