277e559b68175370e85b49e97540f321c1edb0fb
stagas <gstagas@gmail.com>
2023-10-20 13:56:03 +0300

    save

--- src/mouse.ts
+++ src/mouse.ts
@@ -42,9 +42,7 @@ export class Mouse extends Comp {
 
   @fx handle_pointer_event() {
     const { ctx } = $.of(this)
-    const { world, buffer, pointables, input, dims } = $.of(ctx)
-    // const { charWidth } = $.of(dims)
-    // const { mouse } = $.of(input)
+    const { world, buffer, pointables } = $.of(ctx)
     const { pointer } = $.of(world)
     const { time, real } = $.of(pointer)
     $()
