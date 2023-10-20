b0470bde0102a1d12f27d048c6935ea4f4bcbcac
stagas <gstagas@gmail.com>
2023-10-20 18:30:40 +0300

    save

--- src/mouse.ts
+++ src/mouse.ts
@@ -52,7 +52,7 @@ export class Mouse extends Comp {
     const { type, pos } = pointer
     const { linecol: lineCol, downIt, hoverIt } = this
 
-    if (type !== Wheel) {
+    if (type !== Wheel && !misc.isScrolling) {
       buffer.getLineColFromPoint(pos, true, lineCol)
       misc.isTyping = false
     }
