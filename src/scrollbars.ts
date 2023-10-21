3d153040bee2304facac94c02dbcddccb55bd097
stagas <gstagas@gmail.com>
2023-10-21 19:41:27 +0300

    save

--- src/scrollbars.ts
+++ src/scrollbars.ts
@@ -24,45 +24,11 @@ export class Scrollbars extends Comp {
   }
   get renderable(): $<Renderable> {
     $()
-    // const it = this
-    // class ScrollbarsRenderable extends Renderable {
-    //   // @fx trigger_needDraw() {
-    //   //   let needDraw = false
-    //   //   for (const { renderable: r } of it.items) {
-    //   //     if (r.needRender) {
-    //   //       needDraw = true
-    //   //       break
-    //   //     }
-    //   //   }
-    //   //   $()
-    //   //   this.needDraw = needDraw
-    //   // }
-    //   // @fn draw(t: number, c: CanvasRenderingContext2D) {
-    //   //   for (const { renderable: r } of it.items) {
-    //   //     if (!r.isVisible) continue
-
-    //   //     r.needInit && r.initCanvas(r.canvas.c)
-    //   //     r.needRender && r.render()
-    //   //     r.draw(t, c)
-    //   //   }
-    //   //   this.needDraw = false
-    //   // }
-    // }
     return $(new Renderable(this.ctx, this.ctx.renderable.rect, this.ctx.world.canvas))
   }
 
   get pointable() {
     $()
-    // const it = this
-    // class ScrollbarsPointable extends Pointable {
-    //   getItAtPoint(p: Point) {
-    //     for (const item of it.items) {
-    //       if (item.renderable.isVisible && item.pointable.getItAtPoint(p)) {
-    //         return item
-    //       }
-    //     }
-    //   }
-    // }
     return $(new Pointable(this))
   }
 }
