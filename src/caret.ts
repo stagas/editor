2a60e76bcc6fb929eb42bcd7726e7aa8c698ad79
stagas <gstagas@gmail.com>
2023-10-21 20:57:26 +0300

    save

--- src/caret.ts
+++ src/caret.ts
@@ -46,7 +46,7 @@ export class Caret extends Comp {
       }
       @fx update_caret() {
         const { pr, rect: r } = $.of(this)
-        const { blink } = $.of(it)
+        const { blink, linecol } = $.of(it)
         const { pointable: { isFocused } } = $.of(ctx)
         const { lineHeight, charWidth } = $.of(dims)
         $()
