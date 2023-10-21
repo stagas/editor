ffc4b5c75ed8f5471be58cff851e0328c9b7ed73
stagas <gstagas@gmail.com>
2023-10-21 20:48:45 +0300

    save

--- src/active-line.ts
+++ src/active-line.ts
@@ -10,6 +10,7 @@ export class ActiveLine extends Comp {
     const { ctx } = $.of(it)
     const { skin, buffer, dims } = $.of(ctx)
     class ActiveLineRenderable extends Renderable {
+      position = Renderable.Position.Scroll
       viewRect = $(new Rect)
       @fx update_rect() {
         const { rect: r, viewRect: vr } = this
