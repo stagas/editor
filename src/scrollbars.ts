0bd54f7e2a130ddf1998a7ec5faa224517a25801
stagas <gstagas@gmail.com>
2023-10-21 19:01:04 +0300

    save

--- src/scrollbars.ts
+++ src/scrollbars.ts
@@ -1,5 +1,5 @@
 // log.active
-import { $, fx } from 'signal'
+import { $ } from 'signal'
 import { Point } from 'std'
 import { Comp } from './comp.ts'
 import { Pointable } from './pointable.ts'
@@ -19,30 +19,30 @@ export class Scrollbars extends Comp {
   get renderable(): $<Renderable> {
     $()
     const it = this
-    class ScrollbarsRenderable extends Renderable {
-      @fx trigger_needDraw() {
-        let needDraw = false
-        for (const { renderable: r } of it.items) {
-          if (r.needRender) {
-            needDraw = true
-            break
-          }
-        }
-        $()
-        this.needDraw = needDraw
-      }
-      // @fn draw(t: number, c: CanvasRenderingContext2D) {
-      //   for (const { renderable: r } of it.items) {
-      //     if (!r.isVisible) continue
+    // class ScrollbarsRenderable extends Renderable {
+    //   // @fx trigger_needDraw() {
+    //   //   let needDraw = false
+    //   //   for (const { renderable: r } of it.items) {
+    //   //     if (r.needRender) {
+    //   //       needDraw = true
+    //   //       break
+    //   //     }
+    //   //   }
+    //   //   $()
+    //   //   this.needDraw = needDraw
+    //   // }
+    //   // @fn draw(t: number, c: CanvasRenderingContext2D) {
+    //   //   for (const { renderable: r } of it.items) {
+    //   //     if (!r.isVisible) continue
 
-      //     r.needInit && r.initCanvas(r.canvas.c)
-      //     r.needRender && r.render()
-      //     r.draw(t, c)
-      //   }
-      //   this.needDraw = false
-      // }
-    }
-    return $(new ScrollbarsRenderable(this.ctx, this.ctx.renderable.rect, this.ctx.world.canvas))
+    //   //     r.needInit && r.initCanvas(r.canvas.c)
+    //   //     r.needRender && r.render()
+    //   //     r.draw(t, c)
+    //   //   }
+    //   //   this.needDraw = false
+    //   // }
+    // }
+    return $(new Renderable(this.ctx, this.ctx.renderable.rect, this.ctx.world.canvas))
   }
 
   get pointable() {
