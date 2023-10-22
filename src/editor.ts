dba3cd86974b3e9888a0a2d5360af05c6c976f63
stagas <gstagas@gmail.com>
2023-10-22 13:55:39 +0300

    save

--- src/editor.ts
+++ src/editor.ts
@@ -58,33 +58,33 @@ export class Editor extends Scene {
 
   // widgets
   deco: Widget.It[] = [
-    $(new Widget(this), {
-      widgetable: {
-        kind: Widget.Kind.Deco,
-        dim: { p1: { line: 0, col: 1 }, p2: { line: 0, col: 4 } }
-      }
-    }),
-    $(new Widget(this), {
-      widgetable: {
-        kind: Widget.Kind.Deco,
-        dim: { p1: { line: 4, col: 1 }, p2: { line: 4, col: 10 } }
-      }
-    }),
+    // $(new Widget(this), {
+    //   widgetable: {
+    //     kind: Widget.Kind.Deco,
+    //     dim: { p1: { line: 0, col: 1 }, p2: { line: 0, col: 4 } }
+    //   }
+    // }),
+    // $(new Widget(this), {
+    //   widgetable: {
+    //     kind: Widget.Kind.Deco,
+    //     dim: { p1: { line: 4, col: 1 }, p2: { line: 4, col: 10 } }
+    //   }
+    // }),
   ]
   mark: Widget.It[] = []
   sub: Widget.It[] = [
-    $(new Widget(this), {
-      widgetable: {
-        kind: Widget.Kind.Sub,
-        dim: { p1: { line: 8, col: 1 }, p2: { line: 8, col: 4 } }
-      }
-    }),
-    $(new Widget(this), {
-      widgetable: {
-        kind: Widget.Kind.Sub,
-        dim: { p1: { line: 16, col: 1 }, p2: { line: 16, col: 10 } }
-      }
-    }),
+    // $(new Widget(this), {
+    //   widgetable: {
+    //     kind: Widget.Kind.Sub,
+    //     dim: { p1: { line: 8, col: 1 }, p2: { line: 8, col: 4 } }
+    //   }
+    // }),
+    // $(new Widget(this), {
+    //   widgetable: {
+    //     kind: Widget.Kind.Sub,
+    //     dim: { p1: { line: 16, col: 1 }, p2: { line: 16, col: 10 } }
+    //   }
+    // }),
   ]
 
   get renderables(): Renderable.It[] {
