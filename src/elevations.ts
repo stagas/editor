350915de803ef054031965ad98eb210dc0034d42
stagas <gstagas@gmail.com>
2023-10-23 13:11:27 +0300

    save

--- src/elevations.ts
+++ src/elevations.ts
@@ -160,21 +160,21 @@ export class Elevations extends Comp {
           }
         }
 
-        // const latest = eligible.at(-1)
+        const latest = eligible.at(-1)
 
-        // if (latest && !drawnElevations.has(latest)) {
-        //   drawnElevations.add(latest)
-        //   const dr = buffer.fillTextRange(
-        //     c,
-        //     latest,
-        //     colors.fill,
-        //     true,
-        //     2,
-        //     colors.light,
-        //     colors.dark,
-        //   )
-        //   if (dr) dirtyRect.combine(dr)
-        // }
+        if (latest && !drawnElevations.has(latest)) {
+          drawnElevations.add(latest)
+          const dr = buffer.fillTextRange(
+            c,
+            latest,
+            colors.fill,
+            true,
+            2,
+            colors.light,
+            colors.dark,
+          )
+          if (dr) dirtyRect.combine(dr)
+        }
       }
       @fn render(t: number, c: CanvasRenderingContext2D, clear?: boolean) {
         if (this.needDraw) return
@@ -185,9 +185,9 @@ export class Elevations extends Comp {
 
         dirtyRects[0].zero()
         dirtyRects[1].zero()
-        if (clear) {
+        // if (clear) {
           rect.clear(c)
-        }
+        // }
         c.save()
         c.translate(scroll.x, scroll.y)
         it.drawnElevations.clear()
