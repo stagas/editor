2f192ec391d00f4c9fff4fad68dd8254f92d5800
stagas <gstagas@gmail.com>
2023-10-23 12:59:01 +0300

    save

--- src/indicator.ts
+++ src/indicator.ts
@@ -51,7 +51,7 @@ export class Indicator extends Comp {
         // c.save()
         // c.translate(offset.x, offset.y)
         // console.log(rect.x, rect.y)
-        rect.drawImage(canvas.el, c, 1,  true)
+        rect.drawImage(canvas.el, c)
         // c.restore()
         this.needDraw = false
       }
